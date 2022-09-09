f93a284a2235e7f704971ac8f2a7343ffdcd3920
Aleksandr Serikov <Sashas840@gmail.com>
2022-09-09 12:19:00 +0300

    Improved mobile version

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import './index.scss';
-import { Collection } from './Collection';
-import DetailedCollection from './DetailedCollection';
+import React from "react";
+import "./index.scss";
+import { Collection } from "./Collection";
+import DetailedCollection from "./DetailedCollection";
 
 const categories = [
-  { "name": "Все" },
-  { "name": "Лето" },
-  { "name": "Зима" },
-  { "name": "Осень" },
-  { "name": "Архитектура" },
-  { "name": "Самолёты" }
+  { name: "Все" },
+  { name: "Лето" },
+  { name: "Зима" },
+  { name: "Осень" },
+  { name: "Архитектура" },
+  { name: "Самолёты" },
 ];
 
 function App() {
   const [categoryId, setCategoryId] = React.useState(0);
   const [isLoading, setIsLoading] = React.useState(true);
   const [page, setPage] = React.useState(1);
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
   const [collections, setCollections] = React.useState([]);
-  
+
   const [isDetailedViewOpened, setDetailedView] = React.useState(false);
   const [detailedPhotos, setDetailedPhotos] = React.useState([]);
 
   React.useEffect(() => {
     setIsLoading(true);
 
-    const category = categoryId ? `category=${categoryId}` : '';
+    const category = categoryId ? `category=${categoryId}` : "";
 
-    fetch(`https://6307af893a2114bac76922d9.mockapi.io/photos/photos?page=${page}&limit=3&${category}`)
-      .then(res => res.json())
-      .then(json => {
+    fetch(
+      `https://6307af893a2114bac76922d9.mockapi.io/photos/photos?page=${page}&limit=3&${category}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
         setCollections(json);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-        alert('Error while getting data');
-      }).finally(() => setIsLoading(false));
-  }, [categoryId, page]
-  );
+        alert("Error while getting data");
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryId, page]);
 
   const openDetailedView = (images) => {
     setDetailedView(true);
@@ -49,54 +51,71 @@ function App() {
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          {
-            categories.map((el, index) =>
-              <li
-                key={el.name}
-                className={categoryId === index ? 'active' : ''}
-                onClick={() => setCategoryId(index)}>
-                {el.name}
-              </li>)
-          }
+          {categories.map((el, index) => (
+            <li
+              key={el.name}
+              className={categoryId === index ? "active" : ""}
+              onClick={() => setCategoryId(index)}
+            >
+              {el.name}
+            </li>
+          ))}
         </ul>
         <input
           className="search-input"
           placeholder="Поиск по названию"
-          onChange={(value) => setSearchValue(value.target.value)} />
+          onChange={(value) => setSearchValue(value.target.value)}
+        />
       </div>
       <div className="content">
-        {isLoading ? (<h2>Loading...</h2>)
-          : (
-            collections
-              .filter(obj => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
-              .map((el, index) =>
-                <Collection
-                  key={index}
-                  name={el.name}
-                  images={el.photos}
-                  setDetailedView={openDetailedView}
-                />)
-          )
-        }
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : collections.length === 0 ? (
+          <div>
+            <h2>Здесь фотографий нет 😥</h2>
+            <p className="go-back" onClick={() => setPage(1)}>
+              Вернуться назад
+            </p>
+          </div>
+        ) : (
+          !isDetailedViewOpened &&
+          collections
+            .filter((obj) =>
+              obj.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((el, index) => (
+              <Collection
+                key={index}
+                name={el.name}
+                images={el.photos}
+                setDetailedView={openDetailedView}
+              />
+            ))
+        )}
 
-        {isDetailedViewOpened && <DetailedCollection imageLinks={detailedPhotos} setDetailedView={setDetailedView} />}
-
+        {isDetailedViewOpened && (
+          <DetailedCollection
+            imageLinks={detailedPhotos}
+            setDetailedView={setDetailedView}
+          />
+        )}
       </div>
-      <ul className="pagination">
-        {
-          [...Array(4)].map((_, i) => {
+
+      {!isDetailedViewOpened && (
+        <ul className="pagination">
+          {[...Array(4)].map((_, i) => {
             const newPage = ++i;
             return (
               <li
-                className={(newPage) === page ? 'active' : ''}
-                onClick={() => setPage(newPage)}>
+                className={newPage === page ? "active" : ""}
+                onClick={() => setPage(newPage)}
+              >
                 {newPage}
               </li>
             );
-          }
-          )
-        }
-      </ul>
+          })}
+        </ul>
+      )}
     </div>
   );
 }
